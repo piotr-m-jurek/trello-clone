6c07c43d15dac38ab22e10efba8e8573f7757083
Piotr Małecki-Jurek <piotr.m.jurek@gmail.com>
2019-06-14 16:01:39 +0200

    remove prettier dep

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { Route, Switch, Redirect } from "react-router-dom"
+import { Route, Switch } from "react-router-dom"
 
 import { getHistory } from "../store"
 import { ConnectedRouter } from "connected-react-router"
