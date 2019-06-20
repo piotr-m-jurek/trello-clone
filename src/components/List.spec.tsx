6295873f78aa23b783970262c4b359a59e5b2968
Piotr Małecki-Jurek <piotr.m.jurek@gmail.com>
2019-06-20 16:57:16 +0200

    refine list test

--- src/components/List.spec.tsx
+++ src/components/List.spec.tsx
@@ -5,15 +5,19 @@ import { shallow } from "enzyme"
 describe("NewCardForm", () => {
     it("resets input after edition", async () => {
         // tslint:disable-next-line:no-console
-        const form = shallow(<NewCardForm onSubmit={s => console.log(s)} />)
-        form.find(".List__AddButton").simulate("click")
-        const input = () => form.find("input")
-
+        const form = shallow(<NewCardForm onSubmit={_ => null} />)
         const value = "test 1"
+        const input = () => form.find("input")
+        const addButton = () => form.find(".List__AddButton")
+
+        addButton().simulate("click")
         input().simulate("change", { target: { value } })
+
         expect(input().props().value).toEqual(value)
+
         form.find("button").simulate("click")
-        form.find(".List__AddButton").simulate("click")
+        addButton().simulate("click")
+
         expect(input().props().value).toEqual("")
     })
 })
